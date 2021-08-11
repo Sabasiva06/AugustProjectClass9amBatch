228a4294a1798d4a03ccaf3e5f8dad5a8eb62d3a
Sabasiva06 <sabasiva06@gmail.com>
2021-08-11 13:54:10 +0530

    Greens Push

--- src/test/java/org/runner/TestRunner.java
+++ src/test/java/org/runner/TestRunner.java
@@ -12,20 +12,28 @@ import cucumber.api.junit.Cucumber;
 @RunWith(Cucumber.class)
 @CucumberOptions(features = { "src\\test\\resources\\Login.feature" }, glue = {
 		"org.stepdef" }, monochrome = true, dryRun = false, plugin = { "pretty", "html:D:\\9pmCucumberBatch\\target",
-				"json:D:\\9pmCucumberBatch\\target\\fb.json",
-				"junit:D:\\\\9pmCucumberBatch\\\\target\\\\fb.xml","json:D:\\9pmCucumberBatch\\target\\Report\\sample.json" }, 
-				snippets = SnippetType.CAMELCASE)
+				"json:D:\\9pmCucumberBatch\\target\\fb.json", "junit:D:\\\\9pmCucumberBatch\\\\target\\\\fb.xml",
+				"json:D:\\9pmCucumberBatch\\target\\Report\\sample.json" }, snippets = SnippetType.CAMELCASE)
 public class TestRunner {
-	
+
 	@BeforeClass
 	public static void beforeClass() {
 		System.out.println(System.getProperty("ENV"));
 	}
-	
+
 	@AfterClass
 	public static void afterClass() {
 		ReportJVM.generateJVMReport("D:\\9pmCucumberBatch\\target\\Report\\sample.json");
 
+	}
+
+	private void add() {
+		System.out.println("add");
+
+	}
+
+	private void sub() {
+		System.out.println("sub");
 	}
 
 }
